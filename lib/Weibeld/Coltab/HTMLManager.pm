061985c895567c5314ed9930f64e000b64f96b9b
Daniel Weibel <danielmweibel@gmail.com>
2017-04-13 16:43:25 +0700

    Pack HTMLManager and ParseManager into modules
    - Directory structure is adapted: script in bin/ and modules under lib/
    - The lib/ directory is dynamically added to @INC in the script (it
      is necessary only there)
    - The ParseManager module also uses the HTMLManager module. This works
      with a simple `use` statement, as the @INC is already adapted in the
      script

---
+++ lib/Weibeld/Coltab/HTMLManager.pm
@@ -0,0 +1,72 @@
+package Weibeld::Coltab::HTMLManager;
+
+use strict;
+use warnings;
+use HTML::TagTree;
+use File::Map qw(map_file);
+use feature qw(switch);
+no warnings 'experimental::smartmatch';
+
+# Import 'import' so that users of our module can import our subs
+use Exporter qw(import);
+our @EXPORT_OK = qw(init_html set_css add_header
+                    start_new_table add_table_row get_html);
+my $html;
+my $head;
+my $body;
+my $table;
+
+# Initialise a new HTML tree. Must be called before all other subroutines.
+sub init_html {
+    $html= HTML::TagTree->new('html');
+    $head = $html->head();
+    $body = $html->body();
+}
+
+# Add CSS information to HTML tree.
+# Arguments:
+#   $css:     a valid filename
+#   $is_link: bool: true => add link to CSS file; false => include CSS content
+sub set_css {
+    my($css, $is_link) = @_;
+    if ($is_link) {
+        $head->link('', "rel=\"stylesheet\" href=\"$css\"");
+    } else {
+        map_file(my $css_content, $css);
+        $head->style($css_content);
+    }
+}
+
+# Add a header (<h1> to <h6>) to the HTML tree
+sub add_header{
+    my ($level, $text) = @_;
+    given ($level) {
+        when (1) { $body->h1($text); }
+        when (2) { $body->h2($text); }
+        when (3) { $body->h3($text); }
+        when (4) { $body->h4($text); }
+        when (5) { $body->h5($text); }
+        when (6) { $body->h6($text); }
+        default { die "Invalid HTML header level"; }
+    }
+}
+
+# Add a new table to the HTML tree
+sub start_new_table {
+    $table = $body->table();
+}
+
+# Add a new row to the most recently added table of the HTML tree
+sub add_table_row {
+    my $color_code = shift;
+    my $tr = $table->tr();
+    $tr->td('', "class=\"color\" style=\"background-color:$color_code\"");
+    $tr->td($color_code, 'class="code"');
+}
+
+# Return the generated HTMl as a string
+sub get_html{
+    return $html->get_html_text(-1, 0);
+}
+
+1;
---
+++ lib/Weibeld/Coltab/ParseManager.pm
@@ -0,0 +1,61 @@
+package Weibeld::Coltab::ParseManager;
+
+use strict;
+use warnings;
+use Exporter qw(import);
+use Weibeld::Coltab::HTMLManager qw(add_header start_new_table add_table_row);
+
+our @EXPORT_OK = qw(parse_file);
+
+my $is_table_started;
+
+sub parse_file {
+    my $fname = shift;
+    open(my $f, '<', $fname) or die "Can't open file $fname for reading: $!";
+    # Loop through all the lines of the input file
+    while (<$f>) {
+        next if parse_list_item($_);
+        # If it's not a list item and there's an open table, close the table
+        if ($is_table_started) { $is_table_started = 0; }
+        next if parse_header($_);
+    }
+}
+
+sub parse_list_item {
+    my $line = shift;
+    # Test if the line is a list item
+    if ($line =~ /^[*+-]\s+`?\#?([a-f0-9]{3}|[a-f0-9]{6})`?\s*$/i) {
+        if (not $is_table_started) {
+            start_new_table();
+            $is_table_started = 1;
+        }
+        add_table_row("#$1");
+        return 1;
+    }
+}
+
+sub parse_header {
+    my $line = shift;
+    if (is_header($line)) {
+        my %header = get_header($line);
+        add_header($header{level}, $header{text});
+        return 1;
+    }
+}
+
+sub get_header {
+    my $line = shift;
+    for my $i (1 .. 6) {
+        my $pattern = "^" . "#"x$i . "\\s+(.*)\$";
+        if ($line =~ /$pattern/) {
+            return (level => $i, text => $1);
+        }
+    }
+}
+
+sub is_header {
+    my $line = shift;
+    return ($line =~ /^#{1,6}\s/);
+}
+
+1;
