d1f29d17ceb0194da00d818d6e9e922d87504f2d
Daniel Weibel <danielmweibel@gmail.com>
2017-05-04 16:46:55 +0700

    Finalise feature

--- lib/Weibeld/Coltab/ParseManager.pm
+++ lib/Weibeld/Coltab/ParseManager.pm
@@ -4,32 +4,35 @@ use strict;
 use warnings;
 use Carp;
 use Exporter qw(import);
-use Weibeld::Coltab::HTMLManager qw(add_header start_new_table add_table_row);
 
-our(@EXPORT_OK, $VERSION);
+our $VERSION = "0.01";
+our @EXPORT_OK = qw(parse_file);
 
-$VERSION = "0.01";
-@EXPORT_OK = qw(parse_file);
-
-# /^[*+-]\s+`?\#?([a-f0-9]{3}|[a-f0-9]{6})`?\s*$/i
-
-my $is_open_list;
+# Reference to hash with references to functions
 my $callbacks;
+# True if there's an ongoing list in the input file, and false otherwise
+my $is_open_list;
 
+# Primary function of this module. Read input file line by line and call
+# provided callbacks. The first argument is the input file, the second argument
+# is a reference to a hash containing function refs under the following keys:
+#   - on_header_found
+#   - on_list_item_found
+#   - on_list_interrupted
 sub parse_file {
     my $file = shift;
     $callbacks = shift;
     $is_open_list = 0;
 
-    # Loop through all the lines of the file
+    # Loop through all the lines of the input file
     open(my $f, '<', $file) or croak "Can't open file $file for reading: $!";
     while (<$f>) {
         # Test if line is a list item
         if (my $item = _parse_list_item($_)) {
             $callbacks->{on_list_item_found}->($item);
-            $is_open_list = 1 if ($is_open_list == 0);
+            $is_open_list = 1 if (not $is_open_list);
             next;
-        # If line is not a list item, test if an ongoing list is interrupted
+        # If line is not a list item, test if a list has been interrupted
         } else {
             _interrupt_list();
         }
@@ -44,18 +47,15 @@ sub parse_file {
     _interrupt_list();
 }
 
-sub _interrupt_list {
-    if ($is_open_list) {
-        $callbacks->{on_list_interrupted}->();
-        $is_open_list = 0;
-    }
-}
-
+# Test if the passed string is a Markdown list item, if so, return the item text
 sub _parse_list_item {
     my $line = shift;
     return $1 if ($line =~ /^[*+-]\s+(.*)$/);
 }
 
+
+# Test if the passed string is a Markdown header (level 1 to 6), if so, return
+# a reference to a hash with the keys "content" and "level".
 sub _parse_header {
     my $line = shift;
     for my $i (1 .. 6) {
@@ -64,6 +64,14 @@ sub _parse_header {
             my %hash = (level => $i, content => $1);
             return \%hash;
         }
+    }
+}
+
+# If there's a running list, signalize its interruption by through the callback
+sub _interrupt_list {
+    if ($is_open_list) {
+        $callbacks->{on_list_interrupted}->();
+        $is_open_list = 0;
     }
 }
 
